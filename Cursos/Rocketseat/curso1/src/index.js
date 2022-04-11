57ec103ed176fafec43c439c508528238fc4dda0
Luisotee <Luisotaviomgr1@gmail.com>
2022-04-11 20:12:20 -0300

    a

--- Cursos/Rocketseat/curso1/src/index.js
+++ Cursos/Rocketseat/curso1/src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './pages/Home';
+import React from "react";
+import ReactDOM from "react-dom";
+import Home from "./pages/Home";
 
 ReactDOM.render(
   <React.StrictMode>
     <Home />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
--- aula desenvolvimento web/README.md
+++
@@ -1,2 +0,0 @@
-# College
-Luk elsewhere m8
