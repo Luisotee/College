a1163cff6ae6db39568fc0951a1807e345421951
Luisotee <luisotaviomgr1@gmail.com>
2022-04-29 23:35:46 -0300

    a

--- aula desenvolvimento web/projeto-componentes/src/App.js
+++ aula desenvolvimento web/projeto-componentes/src/App.js
@@ -25,7 +25,7 @@ function App() {
           Nome:
           <input
             type="text"
-            id="Name"
+            id="name"
             placeholder="Name"
             value={Name}
             onChange={changeName}
@@ -34,22 +34,22 @@ function App() {
         <label>
           <br />
           Endereço:
-          <input type="text" id="Address" />
+          <input type="text" id="address" placeholder="Endereço" />
         </label>
         <label>
           <br />
           Cidade:
-          <input type="text" id="City" />
+          <input type="text" id="city" placeholder="Cidade" />
         </label>
         <label>
           <br />
           Estado:
-          <input type="text" id="State" />
+          <input type="text" id="state" placeholder="Estado" />
         </label>
         <label>
           <br />
           Numero de telefone:
-          <input type="text" id="CellphoneNumber" />
+          <input type="text" id="cellphoneNumber" placeholder="Numero de telefone"/>
         </label>
         <br></br>
         <input type="submit" value="Enviar"></input>
--- aula desenvolvimento web/projeto-componentes/src/enviarValidar.js
+++ aula desenvolvimento web/projeto-componentes/src/enviarValidar.js
@@ -4,13 +4,17 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export function enviarValidar(e) {
+  let name = document.getElementById("name")
+  let address = document.getElementById('address')
+  let city = document.getElementById('city')
+  let state = document.getElementById('state')
+  let cellphoneNumber = document.getElementById('cellphoneNumber')
+
   toast.configure();
   if (e.preventDefault()) alert("Dados cadastrados");
-  if ((document.getElementById("Name").value === ""))
-    toast.error("Erro. Nome não pode estar vazio");
-  else {   
-    toast.success("Dados Cadastrados! ");
-  } 
+  if (name.value  === "" || address.value  === "" || city.value  === "" || state.value === "")
+    toast.error("Erro. Campo não pode estar vazio");
+  else toast.success("Dados Cadastrados! ");
 }
 
 export function changeName(e) {
